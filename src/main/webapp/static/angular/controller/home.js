87eed403ef611d730e5d8d8d796536805dd45b4d
aortmann <aortmann@despegar.com>
2017-02-05 15:34:50 -0300

    fix add

--- src/main/webapp/static/angular/controller/home.js
+++ src/main/webapp/static/angular/controller/home.js
@@ -29,7 +29,7 @@ app.controller('HomeController', ['$q', '$http', '$location', '$mdDialog', funct
                 homeCtrl.addForm.time,
                 homeCtrl.addForm.feel,
                 homeCtrl.addForm.where,
-                [homeCtrl.addForm.medication],
+                homeCtrl.addForm.medication,
                 homeCtrl.addForm.duration,
                 homeCtrl.addForm.painLevel],
             headers: {
